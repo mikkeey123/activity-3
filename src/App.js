ef59ad360fbec615c136cd407e4c984498f23d45
Mike Edniel <mikeednielm@gmail.com>
2025-10-15 14:45:48 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import Login from './components/Login/Login';
 
 function App() {
+
+    const correctUser = 'mike'
+    const correctPass = '1234'
   return (
     <div>
-      <Login/>
+      <Login Correctusername={correctUser} Correctpassword={correctPass}/>
     </div>
   );
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
 import './Login.css'
+import React, { useState } from 'react';
 
 import user_icon from '../assets/user.png'
 import password_icon from '../assets/password.png'
 
-const Login = ()=>{
+const Login = ({Correctusername,Correctpassword})=>{
     
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        if (username === Correctusername && password === Correctpassword) {
+            alert('Login successful!')
+        } else {
+            alert('Wrong username or password!')
+        }
+    }
     
     return(
         <div className='container'>
@@ -18,20 +28,22 @@ const Login = ()=>{
             <div className="inputs">
                 <div className="input">
                     <img src={user_icon} alt="" />
-                    <input type="text" placeholder='Name' />
+                    <input type="text" 
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)} placeholder='Name' />
                 </div>
 
                 <div className="input">
                     <img src={password_icon} alt="" />
-                    <input type="password" placeholder='Password'/>
+                    <input type="password" 
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    placeholder='Password'/>
                 </div>
             </div>
 
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-
             <div className="submit-container">
-                <div className="submit">Sign-up</div>
-                <div className="submit">Log-in</div>
+                <div className="submit" onClick={handleLogin}>Log-in</div>
             </div>
 
         </div>
